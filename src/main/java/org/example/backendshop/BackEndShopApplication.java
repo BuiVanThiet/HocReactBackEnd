e732453a1714e23f96f39abebe016e27cf472fce
BuiVanThiet <thietbuivan2512@gmail.com>
2024-12-23 20:17:43 +0700

    first commit

---
+++ src/main/java/org/example/backendshop/BackEndShopApplication.java
@@ -0,0 +1,13 @@
+package org.example.backendshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackEndShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/backendshop/BackEndShopApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.backendshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
